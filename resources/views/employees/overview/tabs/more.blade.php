1ad804433ebbfb05fcb3964d96fa0c4d709be3f3
ghassan <ghassan@digitalorder.net>
2026-01-19 11:18:01 +0200

    tests

--- resources/views/employees/overview/tabs/more.blade.php
+++ resources/views/employees/overview/tabs/more.blade.php
@@ -6,7 +6,7 @@
 
                 <!--begin::first-->
 
-                <div class="col-lg-4 grid-margin stretch-card">
+                <div class="col-lg-3 grid-margin stretch-card">
                     <div class="card card-rounded">
                         <div class="card-body">
                             <div class="d-sm-flex  ps-2 align-items-start" style="font-weight: 600">
@@ -76,7 +76,7 @@
                 <!--end::first-->
 
                 <!--begin::secend-->
-                <div class="col-lg-3 grid-margin stretch-card">
+                <div class="col-lg-4 grid-margin stretch-card">
                     <div class="card card-rounded">
                         <div class="card-body">
                             <div class="d-sm-flex align-items-start" style="font-weight: 600">
