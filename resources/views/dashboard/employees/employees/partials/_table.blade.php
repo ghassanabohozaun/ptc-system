1946c1167c9501499b72cdb3df881441e67ce14f
ghassan <ghassan@digitalorder.net>
2026-01-20 13:52:18 +0200

    test

--- resources/views/dashboard/employees/employees/partials/_table.blade.php
+++ resources/views/dashboard/employees/employees/partials/_table.blade.php
@@ -26,6 +26,7 @@
                             <th>#</th>
                             <th>{!! __('employees.full_name') !!}</th>
                             <th>{!! __('employees.personal_id') !!}</th>
+                            <th>{!! __('employees.personal_id') !!}</th>
                             <th>{!! __('employees.gender') !!}</th>
                             <th>{!! __('employees.basic_salary') !!} </th>
                             <th>{!! __('employees.bank_name') !!}</th>
@@ -38,7 +39,8 @@
                             <tr>
                                 <td>{!! $loop->iteration !!}</td>
                                 <td>{!! $employee->EmployeeShortName() !!}</td>
-                                <td>{!! $employee->personal_id !!}</td>
+                                <td>{!! $employee->employeeJobDetails->department->name !!}</td>
+                                <th>{!! __('employees.personal_id') !!}</th>
                                 <td>{!! $employee->EmployeeGender() !!}</td>
                                 <td>{!! $employee->basic_salary !!}
                                     <span class="text-success">{!! $employee->currency !!}</span>
