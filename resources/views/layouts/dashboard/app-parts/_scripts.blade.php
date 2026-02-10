87f891a1350281d9bbf93b500f5d534595678c51
ghassan <ghassan@digitalorder.net>
2026-02-10 15:10:53 +0200

    test

--- resources/views/layouts/dashboard/app-parts/_scripts.blade.php
+++ resources/views/layouts/dashboard/app-parts/_scripts.blade.php
@@ -11,11 +11,11 @@
     @endif
     <!-- END BOOTSTRAP DATEPICKER -->
     <!-- BEGIN PAGE VENDOR JS-->
-    <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/chartist.min.js" type="text/javascript"></script>
-    <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/chartist-plugin-tooltip.min.js" type="text/javascript"></script>
+    {{-- <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/chartist.min.js" type="text/javascript"></script> --}}
+    {{-- <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/chartist-plugin-tooltip.min.js" type="text/javascript"></script>
     <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/raphael-min.js" type="text/javascript"></script>
     <script src="{!! asset('assets/dashbaord') !!}/vendors/js/charts/morris.min.js" type="text/javascript"></script>
-    <script src="{!! asset('assets/dashbaord') !!}/vendors/js/timeline/horizontal-timeline.js" type="text/javascript"></script>
+    <script src="{!! asset('assets/dashbaord') !!}/vendors/js/timeline/horizontal-timeline.js" type="text/javascript"></script> --}}
     <!-- END PAGE VENDOR JS-->
     <!-- BEGIN MODERN JS-->
     <script src="{!! asset('assets/dashbaord') !!}/js/core/app-menu.js" type="text/javascript"></script>
