436f69170c9d3d9ff42ea5e77e545bd1f69e9829
ghassan <ghassan@digitalorder.net>
2026-04-12 13:47:16 +0300

    updates

--- resources/views/dashboard/home/tabs/monthly_reports.blade.php
+++ resources/views/dashboard/home/tabs/monthly_reports.blade.php
@@ -2,15 +2,15 @@
 <div id="recent-transactions" class="row">
     <div class="col-lg-12">
         <div class="card h-100">
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h4 class="card-title">{!! __('monthlyReports.show_latest_monthly_reports') !!}</h4>
-                <div class="heading-elements">
+            <div class="card-header d-flex justify-content-between align-items-center flex-wrap gap-2">
+                <h4 class="card-title m-0">{!! __('monthlyReports.show_latest_monthly_reports') !!}</h4>
+                <div class="d-flex align-items-center gap-2">
                     <button type="button"
-                        class="btn btn-primary mr-1"
+                        class="btn btn-primary btn-sm ml-2 mx-sm-1"
                         data-toggle="modal" data-target="#monthlyReportsEmployeesModal">
                         <i class="la la-users"></i> {!! __('monthlyReports.show_employees') !!}
                     </button>
-                    <a class="btn btn-secondary"
+                    <a class="btn btn-secondary btn-sm"
                         href="{!! route('dashboard.monthlyReports.index') !!}">
                         <i class="la la-link"></i> {!! __('general.show_all') !!}
                     </a>
